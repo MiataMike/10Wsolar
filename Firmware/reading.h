6a73ce982f7bc6ce5a690fceaf16d7d0d01ed335
Mike <mwtvy4@mst.edu>
2020-07-10 21:25:02 -0700

    Added linear correction for reading

--- Firmware/reading.h
+++ Firmware/reading.h
@@ -6,9 +6,14 @@
 #define battPin 	A1
 #define currPin		A2
 
-#define sscale 1
-#define bscale 1
-#define cscale 1
+#define bx 1
+#define cx 1
+#define sx 1
+
+#define bb 0
+#define cb -.02
+#define sb 0
+
 
 float readSolar();
 float readBatt();
